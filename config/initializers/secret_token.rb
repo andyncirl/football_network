bd83a320234a25cd3025b9a8ad4371c92ec84628
andyncirl <andyncirl@gmail.com>
2013-02-10 18:39:16 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FootballNetwork::Application.config.secret_token = '130859d1ea4a438ecbd45ee256305ddb4eedb5e7f9f981a02699e88f846485ef0e89306c8fd40604c1163ae2748b5b31853db36b9a45577e61d6ad49071daa50'
